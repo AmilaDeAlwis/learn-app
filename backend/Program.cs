0de5813717880e1cc69a5cc3915f0cff0eb3b170
AmilaDeAlwis <amilaalwis4@gmail.com>
2024-05-15 18:54:25 +0530

    cosmos database created.

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,13 +1,50 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Azure.Cosmos;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddSingleton<CosmosClient>(serviceProvider =>
+{
+    var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+    var cosmosDbEndpoint = configuration["CosmosDb:Endpoint"];
+    var cosmosDbKey = configuration["CosmosDb:Key"];
+
+    if (string.IsNullOrEmpty(cosmosDbEndpoint) || string.IsNullOrEmpty(cosmosDbKey))
+    {
+        throw new InvalidOperationException("Cosmos DB settings are not configured properly.");
+    }
+    return new CosmosClient(cosmosDbEndpoint, cosmosDbKey);
+});
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Create a Cosmos DB database and container on application startup
+var cosmosClient = app.Services.GetRequiredService<CosmosClient>();
+var databaseResponse = await cosmosClient.CreateDatabaseIfNotExistsAsync("LearnApp");
+var database = databaseResponse.Database;
+
+// Create 'Courses' container
+var coursesContainerResponse = await database.CreateContainerIfNotExistsAsync("Courses", "/Id");
+var coursesContainer = coursesContainerResponse.Container;
+
+// Create 'StudentsInfo' container
+var studentsInfoContainerResponse = await database.CreateContainerIfNotExistsAsync("Students", "/Id");
+var studentsInfoContainer = studentsInfoContainerResponse.Container;
+
+// Create 'CustomQuestions' container
+var CustomQuestionContainerResponse = await database.CreateContainerIfNotExistsAsync("CustomQuestions", "/Id");
+var CustomQuestionContainer = CustomQuestionContainerResponse.Container;
+
+// Create 'AnswerOption' container
+var AnswerOptionContainerResponse = await database.CreateContainerIfNotExistsAsync("AnswerOption", "/Id");
+var AnswerOptionContainer = AnswerOptionContainerResponse.Container;
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
