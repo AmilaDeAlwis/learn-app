e102ae1ed7c6bb76283893afc588ff46e980023a
AmilaDeAlwis <amilaalwis4@gmail.com>
2024-05-15 15:41:04 +0530

    StudentInfoEnum created for Nationality, Recidency, and Gender.

---
+++ backend/Enum/StudentInfoEnum.cs
@@ -0,0 +1,34 @@
+﻿// Enum for Nationality
+public enum Nationality
+{
+    Select, // Default value
+    American,
+    British,
+    Canadian,
+    Chinese,
+    French,
+    German,
+    Indian,
+    Japanese,
+    Russian,
+}
+
+// Enum for Residency Status
+public enum Recidency
+{
+    Select, // Default value
+    Citizen,
+    PermanentResident,
+    TemporaryResident,
+    StudentVisa,
+}
+
+// Enum for Gender
+public enum Gender
+{
+    Select, // Default value
+    Male,
+    Female,
+    NonBinary,
+    Custom,
+}
