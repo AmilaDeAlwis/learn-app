5ab8e41d540d13e6a8f47d6e456f9872aca71489
AmilaDeAlwis <amilaalwis4@gmail.com>
2024-05-15 15:25:56 +0530

    navigation property added more Course model.

--- backend/Models/Course.cs
+++ backend/Models/Course.cs
@@ -11,5 +11,10 @@ namespace backend.Models
         [Required]
         [StringLength(500)]
         public string Description { get; set; }
+
+        // Navigation property for related StudentInfo entries
+        public ICollection<StudentInfo> StudentInfos { get; set; }
+        // Navigation property for related CustomQuestions entries
+        public ICollection<CustomQuestion> CustomQuestions { get; set; }
     }
 }
