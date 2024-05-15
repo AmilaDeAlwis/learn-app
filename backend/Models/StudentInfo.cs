ce0e6bbb6d8831f0b46edb84c1e5284bde6ab12d
AmilaDeAlwis <amilaalwis4@gmail.com>
2024-05-15 15:29:25 +0530

    required fields defined in StudentInfo model

--- backend/Models/StudentInfo.cs
+++ backend/Models/StudentInfo.cs
@@ -1,10 +1,15 @@
-﻿namespace backend.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models
 {
     public class StudentInfo
     {
         public string Id { get; set; } // Unique identifier for the student info document
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
         public string Email { get; set; }
 
         // Optional properties with toggles
