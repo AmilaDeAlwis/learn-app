b4c262ae3bf7343575e5d581cdcd1d021e85952d
AmilaDeAlwis <amilaalwis4@gmail.com>
2024-05-15 16:01:02 +0530

    CustomeQuestion enum created and model updated.

--- backend/Models/CustomQuestion.cs
+++ backend/Models/CustomQuestion.cs
@@ -1,10 +1,11 @@
-﻿namespace backend.Models
+﻿using backend.Enum;
+namespace backend.Models
 {
     public class CustomQuestion
     {
         public string Id { get; set; } // Unique identifier for the custom question document
         public string QuestionText { get; set; }
-        public QuestionType Type { get; set; }
+        public QuestionType QuestionType { get; set; }
 
         // Possible answers for multiple choice questions
         public List<AnswerOption> AnswerOptions { get; set; }
