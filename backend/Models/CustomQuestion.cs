b604c01bec71691664b5c045ad4265348a84892a
AmilaDeAlwis <amilaalwis4@gmail.com>
2024-05-15 15:23:18 +0530

    CustomQuestion model created.

---
+++ backend/Models/CustomQuestion.cs
@@ -0,0 +1,9 @@
+﻿namespace backend.Models
+{
+    public class CustomQuestion
+    {
+        public string Id { get; set; } // Unique identifier for the custom question document
+        public string QuestionText { get; set; }
+        public QuestionType Type { get; set; }
+    }
+}
