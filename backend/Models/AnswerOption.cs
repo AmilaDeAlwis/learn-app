51ff199652285754a8f4ef5f4784328d887257ff
AmilaDeAlwis <amilaalwis4@gmail.com>
2024-05-15 15:32:27 +0530

    referencing to CustomQuestion added in the AnswerOption model.

--- backend/Models/AnswerOption.cs
+++ backend/Models/AnswerOption.cs
@@ -4,6 +4,9 @@
     {
         public string Id { get; set; } // Unique identifier for the answer option document
         public string OptionText { get; set; }
-        
+
+        // Reference to the CustomQuestion ID
+        public string CustomQuestionId { get; set; }
+
     }
 }
