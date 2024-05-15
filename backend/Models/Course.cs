dcf43c92b951466e2a69368430a195baa15621bf
AmilaDeAlwis <amilaalwis4@gmail.com>
2024-05-15 15:17:06 +0530

    Models folder created. Course model created.

---
+++ backend/Models/Course.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models
+{
+    public class Course
+    {
+        public string Id { get; set; } // Unique identifier for the course document
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+        [Required]
+        [StringLength(500)]
+        public string Description { get; set; }
+    }
+}
