1627592bbcd2c2c4e905c5fae6f1304e2520f3f1
AmilaDeAlwis <amilaalwis4@gmail.com>
2024-05-15 15:24:54 +0530

    AnswerOption suppoting model created.

---
+++ backend/Models/AnswerOption.cs
@@ -0,0 +1,9 @@
+﻿namespace backend.Models
+{
+    public class AnswerOption
+    {
+        public string Id { get; set; } // Unique identifier for the answer option document
+        public string OptionText { get; set; }
+        
+    }
+}
